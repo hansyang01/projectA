c8e8bfe5ca2ca0940d47103af38ba7998da26aad
Hans Yang <hans00.yang@gmail.com>
2009-11-03 11:06:54 +0800

    modify code on branch01

--- hello.c
+++ hello.c
@@ -3,11 +3,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+// branch01 's code
 int main()
 {
 
 	int a,b,c;
+	int d,e,f;
 
+
+	
 	printf("Hello, Git!\n");
 	printf("Hello, Git!\n");
 	printf("Hello, Git!\n");
