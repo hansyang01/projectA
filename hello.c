66dd7be194a82403b8d426217f963e4a23d7ca3d
Hans Yang <hans00.yang@gmail.com>
2009-11-03 11:08:14 +0800

    modify code on master

--- hello.c
+++ hello.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+// on master 
 int main()
 {
 
 	int a,b,c;
+	char ss[] = "I am on master ... ok"
 
 	printf("Hello, Git!\n");
 	printf("Hello, Git!\n");
