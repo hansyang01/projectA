5b782e7de19069ce13306228ada7777f0b9e6846
Hans Yang <hans00.yang@gmail.com>
2009-11-03 11:24:10 +0800

    master add new string again

--- hello.c
+++ hello.c
@@ -10,6 +10,7 @@ int main()
 	int a,b,c;
 	char ss[] = "I am on master ... ok"
 	char xx[] = "master is master.. oh yeah"
+	char cc[] = "i am hungry now"
 
 	printf("Hello, Git!\n");
 	printf("Hello, Git!\n");
